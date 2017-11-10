3061b457beb867ebbbf1b0d01715274d998a29e7
Vladimir Novick <vladimirnovick@Vladimirs-MacBook-Pro.local>
2017-11-10 11:38:13 -0600

    demo2

--- index.vr.js
+++ index.vr.js
@@ -33,8 +33,8 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-          <Hello />
-          {/* <RoomSwitcher /> */}
+          {/* <Hello /> */}
+          <RoomSwitcher />
           {/* <Primitives/> */}
           {/* <PrimitivesLit/> */}
           {/* <Models /> */}
